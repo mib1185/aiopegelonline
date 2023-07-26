e413ab19ab76f99aa2f55c277a2002e3de7e3e4b
Michael <35783820+mib1185@users.noreply.github.com>
2023-07-26 20:37:48 +0200

    Add further measurements (#16)

---
+++ aiopegelonline/models.py
@@ -0,0 +1,95 @@
+"""aiopegelonline models."""
+
+from __future__ import annotations
+
+
+class Station:
+    """Representation of a station."""
+
+    def __init__(self, data: dict) -> None:
+        """Initialize station class."""
+        self.uuid: str = data["uuid"]
+        self.name: str = data["longname"]
+        self.agency: str = data["agency"]
+        self.river_kilometer: float | None = data.get("km")
+        self.longitude: float | None = data.get("longitude")
+        self.latitude: float | None = data.get("latitude")
+        self.water_name: str = data["water"]["longname"]
+        self.base_data_url: str = (
+            f"https://www.pegelonline.wsv.de/gast/stammdaten?pegelnr={data['number']}"
+        )
+
+    def as_dict(self) -> dict[str, str | float | None]:
+        """Return data das dict."""
+        return {
+            "uuid": self.uuid,
+            "name": self.name,
+            "agency": self.agency,
+            "river_kilometer": self.river_kilometer,
+            "longitude": self.longitude,
+            "latitude": self.latitude,
+            "water_name": self.water_name,
+            "base_data_url": self.base_data_url,
+        }
+
+
+class CurrentMeasurement:
+    """Representation of a current measurement."""
+
+    def __init__(self, data: dict) -> None:
+        """Initialize current measurment class."""
+        self.uom: str = data["unit"]
+        self.value: float = data["currentMeasurement"]["value"]
+
+    def as_dict(self) -> dict[str, str | float]:
+        """Return data das dict."""
+        return {
+            "uom": self.uom,
+            "value": self.value,
+        }
+
+
+class StationMeasurements:
+    """Representation of station measurements."""
+
+    def __init__(self, data: list[dict]) -> None:
+        """Initialize station measurments class."""
+        self.air_temperature: CurrentMeasurement | None = None
+        self.clearance_height: CurrentMeasurement | None = None
+        self.oxygen_level: CurrentMeasurement | None = None
+        self.ph_value: CurrentMeasurement | None = None
+        self.water_speed: CurrentMeasurement | None = None
+        self.water_flow: CurrentMeasurement | None = None
+        self.water_level: CurrentMeasurement | None = None
+        self.water_temperature: CurrentMeasurement | None = None
+
+        for measurement in data:
+            if measurement["shortname"] == "DFH":
+                self.clearance_height = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "LT":
+                self.air_temperature = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "O2":
+                self.oxygen_level = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "PH":
+                self.ph_value = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "Q":
+                self.water_flow = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "VA":
+                self.water_speed = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "W":
+                self.water_level = CurrentMeasurement(measurement)
+            elif measurement["shortname"] == "WT":
+                self.water_temperature = CurrentMeasurement(measurement)
+
+    def as_dict(self) -> dict[str, CurrentMeasurement | None]:
+        """Return data das dict."""
+        return {
+            "air_temperature": self.air_temperature,
+            "clearance_height": self.clearance_height,
+            "oxygen_level": self.oxygen_level,
+            "ph_value": self.ph_value,
+            "water_speed": self.water_speed,
+            "water_flow": self.water_flow,
+            "water_level": self.water_level,
+            "water_temperature": self.water_temperature,
+        }
--- tests/test_aiopegelonline.py
+++ tests/test_aiopegelonline.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 import pytest
 from aiohttp import ClientError
 
-from aiopegelonline import CurrentMeasurement, Station
 from aiopegelonline.exceptions import PegelonlineDataError
+from aiopegelonline.models import Station, StationMeasurements
 
 
 @pytest.mark.asyncio
@@ -17,30 +17,29 @@ async def test_get_all_stations(mock_pegelonline):
 
     station = stations["70272185-xxxx-xxxx-xxxx-43bea330dcae"]
     assert isinstance(station, Station)
-    assert station.uuid == "70272185-xxxx-xxxx-xxxx-43bea330dcae"
-    assert station.name == "DRESDEN"
-    assert station.agency == "STANDORT DRESDEN"
-    assert station.river_kilometer == 55.63
-    assert station.longitude == 13.738831783620384
-    assert station.latitude == 51.054459765598125
-    assert station.water_name == "ELBE"
-    assert (
-        station.base_data_url
-        == "https://www.pegelonline.wsv.de/gast/stammdaten?pegelnr=501060"
-    )
+    assert station.as_dict() == {
+        "uuid": "70272185-xxxx-xxxx-xxxx-43bea330dcae",
+        "name": "DRESDEN",
+        "agency": "STANDORT DRESDEN",
+        "river_kilometer": 55.63,
+        "longitude": 13.738831783620384,
+        "latitude": 51.054459765598125,
+        "water_name": "ELBE",
+        "base_data_url": "https://www.pegelonline.wsv.de/gast/stammdaten?pegelnr=501060",
+    }
 
-    station = stations["706e5110-xxxx-xxxx-xxxx-c071fcb492ec"]
+    station = stations["915d76e1-xxxx-xxxx-xxxx-4d144cd771cc"]
     assert isinstance(station, Station)
-    assert station.uuid == "706e5110-xxxx-xxxx-xxxx-c071fcb492ec"
-    assert station.name == "HAMBURG-HARBURG"
-    assert station.agency == "HAMBURG PORT AUTHORITY"
-    assert station.river_kilometer == 615
-    assert station.longitude == 9.991814826063601
-    assert station.latitude == 53.472725901227285
-    assert station.water_name == "ELBE"
+    assert station.uuid == "915d76e1-xxxx-xxxx-xxxx-4d144cd771cc"
+    assert station.name == "WÜRZBURG"
+    assert station.agency == "SCHWEINFURT"
+    assert station.river_kilometer == 251.97
+    assert station.longitude == 9.925968763247354
+    assert station.latitude == 49.79620901036012
+    assert station.water_name == "MAIN"
     assert (
         station.base_data_url
-        == "https://www.pegelonline.wsv.de/gast/stammdaten?pegelnr=5952025"
+        == "https://www.pegelonline.wsv.de/gast/stammdaten?pegelnr=24300600"
     )
 
 
@@ -107,11 +106,53 @@ async def test_get_station_details_connection_error(mock_pegelonline):
 
 
 @pytest.mark.asyncio
-async def test_get_station_measurement(mock_pegelonline):
-    """Test async_get_station_measurement."""
-    measurement = await mock_pegelonline.async_get_station_measurement(
-        "70272185-xxxx-xxxx-xxxx-43bea330dcae"
+async def test_get_station_measurements(mock_pegelonline):
+    """Test async_get_station_measurements."""
+    measurement = await mock_pegelonline.async_get_station_measurements(
+        "915d76e1-xxxx-xxxx-xxxx-4d144cd771cc"
     )
-    assert isinstance(measurement, CurrentMeasurement)
-    assert measurement.uom == "cm"
-    assert measurement.value == 60
+    assert isinstance(measurement, StationMeasurements)
+    assert measurement.air_temperature is not None
+    assert measurement.air_temperature.uom == "°C"
+    assert measurement.air_temperature.value == 21.2
+    assert measurement.clearance_height is None
+    assert measurement.oxygen_level is not None
+    assert measurement.oxygen_level.uom == "mg/l"
+    assert measurement.oxygen_level.value == 8.4
+    assert measurement.ph_value is not None
+    assert measurement.ph_value.uom == "--"
+    assert measurement.ph_value.value == 8.1
+    assert measurement.water_speed is not None
+    assert measurement.water_speed.uom == "m/s"
+    assert measurement.water_speed.value == 0.58
+    assert measurement.water_flow is not None
+    assert measurement.water_flow.uom == "m³/s"
+    assert measurement.water_flow.value == 102
+    assert measurement.water_level is not None
+    assert measurement.water_level.uom == "cm"
+    assert measurement.water_level.value == 159
+    assert measurement.water_temperature is not None
+    assert measurement.water_temperature.uom == "°C"
+    assert measurement.water_temperature.value == 22.1
+
+    measurement = await mock_pegelonline.async_get_station_measurements(
+        "07374faf-xxxx-xxxx-xxxx-adc0e0784c4b"
+    )
+    assert isinstance(measurement, StationMeasurements)
+    assert measurement.clearance_height is not None
+    assert measurement.clearance_height.uom == "cm"
+    assert measurement.clearance_height.value == 715
+    assert measurement.clearance_height.as_dict() == {
+        "uom": "cm",
+        "value": 715,
+    }
+    assert measurement.as_dict() == {
+        "air_temperature": None,
+        "clearance_height": measurement.clearance_height,
+        "oxygen_level": None,
+        "ph_value": None,
+        "water_speed": None,
+        "water_flow": None,
+        "water_level": None,
+        "water_temperature": None,
+    }
